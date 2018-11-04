2d62e17fedb122eb48f71b6e5eaa6e9bde1442d0
Haliza Hayyu Firdausy <halizahayyu@gmail.com>
2018-11-04 20:51:48 +0700

    Commit All (Class Penginapan.java)

---
+++ src/remedKuis1/hotel/Penginapan.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package remedKuis1.hotel;
+
+/**
+ *
+ * @author Haliza
+ */
+public class Penginapan {
+    private String noKamar;
+    private Penyewa penyewa;
+
+    // Deklarasi Konstruktor Class Penginapan
+    Penginapan(String noKamar) {
+        this.noKamar = noKamar;
+    }
+
+    // getter no kamar
+    public String getNoKamar() {
+        return noKamar;
+    }
+    
+    // setter no kamar
+    public void setNoKamar(String noKamar) {
+        this.noKamar = noKamar;
+    }
+
+    // getter penyewa
+    public Penyewa getPenyewa() {
+        return penyewa;
+    }
+
+    //setter penyewa
+    public void setPenyewa(Penyewa penyewa) {
+        this.penyewa = penyewa;
+    }
+    
+    // output
+    public String info(){
+        String info = "";
+        info += "No. Kamar                  : "+noKamar+"\n";
+        if(this.penyewa != null){
+            info += "Customer               : \n"+penyewa.info()+"\n";
+        }
+        return info;
+    }
+    
+}
